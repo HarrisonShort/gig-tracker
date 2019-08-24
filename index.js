846615f87b490be63816f875a4beb897be8c7833
HarrisonShort <work.harrisonshort@gmail.com>
2019-08-24 19:53:07 +1000

    ADD jumping between months, functionality of buttons, and hiding those buttons when necessary

--- index.js
+++ index.js
@@ -4,9 +4,13 @@
 */
 
 const domStrings = {
-    monthDropdown: "monthDropdown",
-    yearDropdown: "yearDropdown"
+    monthDropdown: "dropdown-month",
+    yearDropdown: "dropdown-year",
+    previousButton: "button-previous",
+    nextButton: "button-next"
 }
+const lowestYear = 1950;
+const highestYear = new Date().getFullYear() + 5;
 
 const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
@@ -27,10 +31,9 @@ function populateMonthDropdown() {
 
 // https://stackoverflow.com/questions/20873302/html-looping-option-values-in-drop-down-list
 function populateYearDropdown() {
-    const lowestYear = 1950;
     const currentYear = new Date().getFullYear();
     let yearOptions = "";
-    for (let year = lowestYear; year <= currentYear + 5; year++) {
+    for (let year = lowestYear; year <= highestYear; year++) {
         if (year === currentYear) {
             yearOptions += `<option value=${year} selected>` + year + "</option>";
         } else {
@@ -40,16 +43,14 @@ function populateYearDropdown() {
     return yearOptions;
 }
 
-function populateCalendarCells() {
+function populateCalendarCells(year, month) {
     const maxRows = 6;
     const maxColumns = 7;
-    const currentSelection = getCurrentDropdownSelections();
     const calendar = document.getElementById("calendar");
     calendar.innerHTML = "";
-    //console.log(`${currentSelection[0]} ${currentSelection[1]}`);
 
-    let firstDay = new Date(currentSelection[1], currentSelection[0]).getDay() - 1;
-    let daysInMonth = 32 - new Date(currentSelection[1], currentSelection[0], 32).getDate();
+    let firstDay = new Date(year, month).getDay() - 1;
+    let daysInMonth = 32 - new Date(year, month, 32).getDate();
 
     let date = 1;
     let columnsLeft;
@@ -92,18 +93,67 @@ function createCell(row, text) {
 /* 
 * Gets the current month and year selections from the dropdown.
 */
-function getCurrentDropdownSelections() {
-    const month = document.getElementById(domStrings.monthDropdown).value;
+function getDropdownValues() {
     const year = document.getElementById(domStrings.yearDropdown).value;
-    return [parseInt(month), parseInt(year)];
+    const month = document.getElementById(domStrings.monthDropdown).value;
+    return [parseInt(year), parseInt(month)];
 }
 
+function setDropdownValues(yearIndex, monthIndex) {
+    document.getElementById(domStrings.yearDropdown).value = yearIndex;
+    document.getElementById(domStrings.monthDropdown).value = monthIndex;
+}
+
+function jumpToDropdownSelections() {
+    const dropdownValues = getDropdownValues();
+    populateCalendarCells(dropdownValues[0], dropdownValues[1]);
+    checkButtonVisibility(dropdownValues[0], dropdownValues[1]);
+}
+
+function jumpToPreviousMonth() {
+    const dropdownValues = getDropdownValues();
+    if (dropdownValues[1] == 0) {
+        dropdownValues[0] -= 1;
+        dropdownValues[1] = 12;
+    }
+    setDropdownValues(dropdownValues[0], dropdownValues[1] - 1);
+    populateCalendarCells(dropdownValues[0], dropdownValues[1] - 1);
+    checkButtonVisibility(dropdownValues[0], dropdownValues[1] - 1);
+}
+
+function jumpToNextMonth() {
+    const dropdownValues = getDropdownValues();
+    if (dropdownValues[1] == 11) {
+        dropdownValues[0] += 1;
+        dropdownValues[1] = -1;
+    }
+    setDropdownValues(dropdownValues[0], dropdownValues[1] + 1);
+    populateCalendarCells(dropdownValues[0], dropdownValues[1] + 1);
+    checkButtonVisibility(dropdownValues[0], dropdownValues[1] + 1);
+}
+
+function checkButtonVisibility(year, month) {
+    if (year === highestYear && month === 11) {
+        document.getElementById(domStrings.nextButton).style.visibility = "hidden";
+    } else if (year === lowestYear && month === 0) {
+        document.getElementById(domStrings.previousButton).style.visibility = "hidden";
+    } else {
+        document.getElementById(domStrings.nextButton).style.visibility = "visible";
+        document.getElementById(domStrings.previousButton).style.visibility = "visible";
+    }
+}
+
+/* 
+* Sets up the event listeners for the previous and next buttons.
+*/
 function setUpEventListeners() {
-
+    document.getElementById(domStrings.previousButton).addEventListener('click', jumpToPreviousMonth);
+    document.getElementById(domStrings.nextButton).addEventListener('click', jumpToNextMonth);
+    document.getElementById(domStrings.monthDropdown).addEventListener('change', jumpToDropdownSelections)
+    document.getElementById(domStrings.yearDropdown).addEventListener('change', jumpToDropdownSelections)
 }
 
-function
-
-    document.getElementById(domStrings.monthDropdown).innerHTML = populateMonthDropdown();
+setUpEventListeners();
+document.getElementById(domStrings.monthDropdown).innerHTML = populateMonthDropdown();
 document.getElementById(domStrings.yearDropdown).innerHTML = populateYearDropdown();
-populateCalendarCells(getCurrentDropdownSelections());
+jumpToDropdownSelections();
