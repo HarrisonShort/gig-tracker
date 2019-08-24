e790f471b48f26a7410d5eeba5ebe42cd84bbb95
HarrisonShort <work.harrisonshort@gmail.com>
2019-08-24 18:36:58 +1000

    ADD displaying the calendar cells correctly

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
+/*
+* Much of the code here is adapted from:
+* https://medium.com/@nitinpatel_20236/challenge-of-building-a-calendar-with-pure-javascript-a86f1303267d
+*/
+
 const domStrings = {
     monthDropdown: "monthDropdown",
     yearDropdown: "yearDropdown"
 }
 
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
 function populateMonthDropdown() {
-    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     const currentMonth = new Date().getMonth();
     let monthOptions;
     let monthIndex = 0;
     months.forEach(month => {
         if (monthIndex === currentMonth) {
-            monthOptions += `<option value=${month} selected>` + month + "</option>"
+            monthOptions += `<option value=${monthIndex} selected>` + month + "</option>"
         } else {
-            monthOptions += `<option value=${month}>` + month + "</option>"
+            monthOptions += `<option value=${monthIndex}>` + month + "</option>"
         }
         monthIndex++;
     });
@@ -24,7 +30,7 @@ function populateYearDropdown() {
     const lowestYear = 1950;
     const currentYear = new Date().getFullYear();
     let yearOptions = "";
-    for (var year = lowestYear; year <= currentYear + 5; year++) {
+    for (let year = lowestYear; year <= currentYear + 5; year++) {
         if (year === currentYear) {
             yearOptions += `<option value=${year} selected>` + year + "</option>";
         } else {
@@ -34,20 +40,70 @@ function populateYearDropdown() {
     return yearOptions;
 }
 
-//https://medium.com/@nitinpatel_20236/challenge-of-building-a-calendar-with-pure-javascript-a86f1303267d
-function populateCalendarSquares() {
+function populateCalendarCells() {
+    const maxRows = 6;
+    const maxColumns = 7;
     const currentSelection = getCurrentDropdownSelections();
-    console.log(`${currentSelection[0]} ${currentSelection[1]}`);
-    //let firstDay = (new Date().getDay())
+    const calendar = document.getElementById("calendar");
+    calendar.innerHTML = "";
+    //console.log(`${currentSelection[0]} ${currentSelection[1]}`);
+
+    let firstDay = new Date(currentSelection[1], currentSelection[0]).getDay() - 1;
+    let daysInMonth = 32 - new Date(currentSelection[1], currentSelection[0], 32).getDate();
+
+    let date = 1;
+    let columnsLeft;
+    for (let i = 0; i < maxRows; i++) {
+        let row = document.createElement("tr");
+        columnsLeft = 7;
+
+        for (let j = 0; j < maxColumns; j++) {
+            // Need to move the first day to the correct position
+            // in the case that it is less than 0
+            if (firstDay < 0) {
+                firstDay = 6;
+            }
+
+            if (i === 0 && j < firstDay) {
+                createCell(row, "");
+            }
+            else if (date > daysInMonth && columnsLeft !== 0) {
+                createCell(row, "");
+            } else if (columnsLeft !== 0) {
+                createCell(row, date);
+                date++;
+            }
+            columnsLeft--;
+        }
+        calendar.appendChild(row);
+    }
 }
 
+/* 
+* Creates an individual cell of the calendar.
+*/
+function createCell(row, text) {
+    let cell = document.createElement("td");
+    let cellText = document.createTextNode(text);
+    cell.appendChild(cellText);
+    row.appendChild(cell);
+}
+
+/* 
+* Gets the current month and year selections from the dropdown.
+*/
 function getCurrentDropdownSelections() {
-    month = document.getElementById(domStrings.monthDropdown).value;
-    year = document.getElementById(domStrings.yearDropdown).value;
-    let monthYear = [month, year];
-    return monthYear;
+    const month = document.getElementById(domStrings.monthDropdown).value;
+    const year = document.getElementById(domStrings.yearDropdown).value;
+    return [parseInt(month), parseInt(year)];
 }
 
-document.getElementById(domStrings.monthDropdown).innerHTML = populateMonthDropdown();
+function setUpEventListeners() {
+
+}
+
+function
+
+    document.getElementById(domStrings.monthDropdown).innerHTML = populateMonthDropdown();
 document.getElementById(domStrings.yearDropdown).innerHTML = populateYearDropdown();
-populateCalendarSquares(getCurrentDropdownSelections());
+populateCalendarCells(getCurrentDropdownSelections());
