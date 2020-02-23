76b741d16044d6e507cef11575bca266d63064b1
HarrisonShort <work.harrisonshort@gmail.com>
2020-02-24 07:08:12 +1000

    ADD main page template

--- gig-tracker/src/App.js
+++ gig-tracker/src/App.js
@@ -1,16 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
 import "bootstrap/dist/css/bootstrap.min.css";
+
+import CreateGig from "./components/create-gig.component";
+import EditGig from "./components/edit-gig.component";
+import GigList from "./components/gig-list.component";
+
+import logo from "./logo.svg"
 
 class App extends Component {
     render() {
         return (
             <Router>
-                <Route path="/" exact component={GigList} />
-                <Route path="/edit/:id" component={EditGig} />
-                <Route path="/create" component={CreateGig} />
                 <div className="container">
-                    <h2>Gig Tracker</h2>
+                    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                        <a className="navbar-brand" target="_blank">
+                            <img src={logo} width="30" height="30" href="http://google.com" />
+                        </a>
+                        <Link to="/" className="navbar-brand">Gig Tracker</Link>
+                        <div className="collpase navbar-collapse">
+                            <ul className="navbar-nav mr-auto">
+                                <li className="navbar-item">
+                                    <Link to="/" className="nav-link">Calendar</Link>
+                                </li>
+                                <li className="navbar-item">
+                                    <Link to="/create" className="nav-link">Add Gig</Link>
+                                </li>
+                            </ul>
+                        </div>
+                    </nav>
+                    <br />
+                    <Route path="/" exact component={GigList} />
+                    <Route path="/edit/:id" component={EditGig} />
+                    <Route path="/create" component={CreateGig} />
                 </div>
             </Router>
         )
