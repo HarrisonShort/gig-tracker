770b4062dbde0fbc3de905b1c8861a79c7adcc39
HarrisonShort <work.harrisonshort@gmail.com>
2020-05-15 13:39:09 +1000

    Updates

---
+++ precache-manifest.c3542230e761ec42fb28f30bd1bf554b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "860f21f431e7b42a3fa9e1515557066b",
+    "url": "/gig-tracker/index.html"
+  },
+  {
+    "revision": "9293a3f767ba79bd97a0",
+    "url": "/gig-tracker/static/css/2.69004b04.chunk.css"
+  },
+  {
+    "revision": "4766915d8ae59e512b4d",
+    "url": "/gig-tracker/static/css/main.f22b270a.chunk.css"
+  },
+  {
+    "revision": "9293a3f767ba79bd97a0",
+    "url": "/gig-tracker/static/js/2.212331a3.chunk.js"
+  },
+  {
+    "revision": "299cba5f6c747cbc295097594263041b",
+    "url": "/gig-tracker/static/js/2.212331a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4766915d8ae59e512b4d",
+    "url": "/gig-tracker/static/js/main.b76a3231.chunk.js"
+  },
+  {
+    "revision": "45c8690c09896718d88b",
+    "url": "/gig-tracker/static/js/runtime-main.f54170b8.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/gig-tracker/static/media/logo.ee7cd8ed.svg"
+  }
+]);
