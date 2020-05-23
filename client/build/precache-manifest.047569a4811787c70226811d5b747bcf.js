8f1d6ba49b930c3e126ce0070f1ca1d941659267
HarrisonShort <work.harrisonshort@gmail.com>
2020-05-23 23:02:51 +1000

    CHANGE folder structure

---
+++ client/build/precache-manifest.047569a4811787c70226811d5b747bcf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24bff69d8aa5413bd5b5067c551d091f",
+    "url": "/gig-tracker/index.html"
+  },
+  {
+    "revision": "846542be1a34ca22cf68",
+    "url": "/gig-tracker/static/css/2.69004b04.chunk.css"
+  },
+  {
+    "revision": "7b98f1b9b54aae2a4153",
+    "url": "/gig-tracker/static/css/main.f22b270a.chunk.css"
+  },
+  {
+    "revision": "846542be1a34ca22cf68",
+    "url": "/gig-tracker/static/js/2.f804dba2.chunk.js"
+  },
+  {
+    "revision": "299cba5f6c747cbc295097594263041b",
+    "url": "/gig-tracker/static/js/2.f804dba2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b98f1b9b54aae2a4153",
+    "url": "/gig-tracker/static/js/main.4c2a4b51.chunk.js"
+  },
+  {
+    "revision": "45c8690c09896718d88b",
+    "url": "/gig-tracker/static/js/runtime-main.f54170b8.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/gig-tracker/static/media/logo.ee7cd8ed.svg"
+  }
+]);
