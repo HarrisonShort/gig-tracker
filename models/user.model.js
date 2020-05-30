73853ce19b5097b1f0c6df62e8e16d449ad3d833
HarrisonShort <work.harrisonshort@gmail.com>
2020-05-30 11:46:51 +1000

    ADD user model

---
+++ models/user.model.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Creates the data structure for a Gig.
+let User = new Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    dateSignedUp: {
+        type: String,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('User', User);
