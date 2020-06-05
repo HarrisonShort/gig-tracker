a693d8be07b4c6eb52eb23bf90453a9657404f59
HarrisonShort <work.harrisonshort@gmail.com>
2020-06-05 19:00:22 +1000

    ADD removing userID from localstorage when logging out

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -61,6 +61,7 @@ export const setUserLoading = () => {
 export const logoutUser = () => dispatch => {
     // Remove token from local storage
     localStorage.removeItem("jwtToken");
+    localStorage.removeItem("userID");
     // Remove auth header for future requests
     setAuthToken(false);
     // Set current user to empty object {} which will set isAuthenticated to false
